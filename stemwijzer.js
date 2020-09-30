8360c4687eeb9de8e4baa991e941fe9394b2a2ad
Bram van Ballegooijen <99046330@mydavinci.nl>
2020-09-30 11:25:33 +0200

    added vragen pagina

---
+++ stemwijzer.js
@@ -0,0 +1,19 @@
+// Stemwijzer // // Stemwijzer // // Stemwijzer // 
+
+
+startButton.onclick = load;
+
+document.getElementById('vraagText').style.display ='none'
+
+function load(){
+	document.getElementById("blueText").innerHTML = 'onderwerp vraag'
+
+	document.getElementById('vraagText').style.display ='inline-block'
+	document.getElementById('stemWijzerHeader').style.display ='none'
+	document.getElementById('logo').style.display ='none'
+	document.getElementById('beperking').style.display ='none'
+	document.getElementById('cookies').style.display ='none'
+	document.getElementById('info').style.display ='none'
+
+
+}
