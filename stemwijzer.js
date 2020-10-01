aca17f79d1124a93053f8b7aa7089701b85ffe95
Bram van Ballegooijen <99046330@mydavinci.nl>
2020-10-01 09:04:59 +0200

    added vraag buttons

--- stemwijzer.js
+++ stemwijzer.js
@@ -4,9 +4,14 @@
 startButton.onclick = load;
 
 document.getElementById('vraagText').style.display ='none'
+document.getElementById('eens').style.display ='none'
+document.getElementById('oneens').style.display ='none'
+document.getElementById('beide').style.display ='none'
 
 function load(){
 	document.getElementById("blueText").innerHTML = 'onderwerp vraag'
+
+	document.getElementById('startButton').style.display ='none'
 
 	document.getElementById('vraagText').style.display ='inline-block'
 	document.getElementById('stemWijzerHeader').style.display ='none'
@@ -14,6 +19,11 @@ function load(){
 	document.getElementById('beperking').style.display ='none'
 	document.getElementById('cookies').style.display ='none'
 	document.getElementById('info').style.display ='none'
+
+	document.getElementById('eens').style.display ='inline-block'
+	document.getElementById('oneens').style.display ='inline-block'
+	document.getElementById('beide').style.display ='inline-block'
+
 
 
 }
